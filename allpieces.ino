1e61540ad81ed97e5b32c7822276eaad7797e125
alexfilisanu <mihaifilisanu@gmail.com>
2023-05-29 03:25:39 +0300

    added interrupts()

--- allpieces.ino
+++ allpieces.ino
@@ -14,17 +14,13 @@ const int forceThreshold = 50; // Threshold for significant force change
 MFRC522 mfrc522(SS_PIN, RST_PIN);
 LiquidCrystal lcd(6, 7, 2, 3, 4, 5);
 FSR fsr(forceSensorPin);
-unsigned long timerStartTime = 0;  // Variable to store the timer start time
-unsigned long lastTimerValue = 0;  // Variable to store the last timer value
+volatile unsigned long timerStartTime = 0;  // Variable to store the timer start time
+volatile unsigned long lastTimerValue = 0;  // Variable to store the last timer value
 int initialForceValue = 200; // Initial force value for comparison
 bool isAuthorized = false; // Flag to track card authorization
-bool isForceChangeDetected = false;  // Flag to track significant force change
+volatile bool isForceChangeDetected = false;  // Flag to track significant force change
 bool hasPlayedVictoryMelody = false;
-int isSecondForceChangeDetected = 0;
-
-const int melody[] = {659, 659, 659, 523, 659, 783, 392, 523, 392, 330, 440, 494, 466, 440, 392, 659, 784, 880, 698, 784, 659, 523, 587, 494, 523, 392};
-const int noteDurations[] = {250, 250, 500, 250, 250, 500, 250, 250, 250, 250, 250, 250, 250, 250, 250, 250, 250, 250, 250, 250, 250, 250, 250, 250, 500};
-
+volatile int isSecondForceChangeDetected = 0;
 
 void setup()
 {
@@ -35,6 +31,8 @@ void setup()
 
   pinMode(buzzerPin, OUTPUT);          // Initialize the buzzer pin as output
   pinMode(forceSensorPin, INPUT);      // Initialize the force sensor pin as input
+
+  attachInterrupt(digitalPinToInterrupt(forceSensorPin), forceChangeInterrupt, CHANGE);
 }
 
 void loop()
@@ -53,31 +51,21 @@ void loop()
   if ((fsr.getForce() - initialForceValue >= forceThreshold) || (fsr.getForce() - initialForceValue <= forceThreshold) && timerStartTime == 0)
   {
     // Start the timer
+    cli();
     timerStartTime = millis();
+    sei();
   }
-
-  // Check if the force value has changed significantly and the timer is already started
+   // Check if the force value has changed significantly and the timer is already started
   if (timerStartTime != 0 && (fsr.getForce() - initialForceValue >= forceThreshold) || (fsr.getForce() - initialForceValue <= forceThreshold) && !isForceChangeDetected)
   {
     // Stop the timer and store the last timer value
+    cli();
     lastTimerValue = millis() - timerStartTime;
     timerStartTime = 0;
     isForceChangeDetected = true;
-  //  if (isForceChangeDetected && !hasPlayedVictoryMelody && isAuthorized)
-  // {
-  //   // playVictoryMelody();  // Play the victory melody
-  //   hasPlayedVictoryMelody = true;  // Set the flag to true
-  //    activateAuthorizedSound();
-  // }   
-  Serial.print(lastTimerValue);
-  isSecondForceChangeDetected++;
-    if (isAuthorized && isSecondForceChangeDetected == 1 && lastTimerValue != timerStartTime) {
-    isSecondForceChangeDetected = true;
-    
-    // activateAuthorizedSound();
+    sei();
   }
-  }
-isSecondForceChangeDetected = 0;
+
   // Display the timer on the LCD and perform other actions if the timer is started and authorized
   if (isAuthorized && timerStartTime != 0)
   {
@@ -141,41 +129,30 @@ isSecondForceChangeDetected = 0;
     lcd.setCursor(0, 1);
     lcd.print("Authorized");
     activateAuthorizedSound();  // Activate the buzzer for 1 second
-    // timerStartTime = millis();  // Start the timer
+    cli();
     timerStartTime = 0;
     isForceChangeDetected = false;
     isAuthorized = true; // Set the authorization flag to true
+    sei();
   }
   else
   {
     lcd.setCursor(0, 1);
     lcd.print("Access denied");
     activateDeniedSound();
-    float forceValue = fsr.getForce();
-    // lcd.setCursor(0, 1);
-    // lcd.print("Force: ");
-    // lcd.print(forceValue);
-    // // initialForceValue = forceValue; // Update the initial force value for comparison
     delay(2000);
     lcd.clear();
     setup();
+    cli();
     timerStartTime = 0;
     isForceChangeDetected = false;
     isAuthorized = false; // Set the authorization flag to false
+    sei();
   }
 }
 
-// void activateBuzzer(int duration)
-// {
-//   tone(buzzerPin, 1000);  // Generate a tone of 1000Hz on the buzzer pin
-//   delay(duration);       // Wait for the specified duration
-//   noTone(buzzerPin);     // Stop the tone
-// }
-
 void activateAuthorizedSound()
 {
-  // tone(buzzerPin, 1000, 1000);  // Generate a tone of 1000Hz on the buzzer pin for 1 second
-  // Play an interesting sound for authorized
   tone(buzzerPin, 1000, 250);
   delay(250);
   tone(buzzerPin, 1500, 250);
@@ -186,16 +163,15 @@ void activateAuthorizedSound()
 
 void activateDeniedSound()
 {
-  // tone(buzzerPin, 2000, 1000);  // Generate a tone of 2000Hz on the buzzer pin for 1 second
-  // Play the denied sound effect
   tone(buzzerPin, 400, 1000);
 }
 
-void playVictoryMelody() {
-  for (int i = 0; i < sizeof(melody) / sizeof(melody[0]); i++) {
-    int noteDuration = 1000 / noteDurations[i];
-    tone(buzzerPin, melody[i], noteDuration);
-    delay(noteDuration * 1.3);   // Add a small delay between notes for better separation
-    noTone(buzzerPin);
+void forceChangeInterrupt()
+{
+  if (timerStartTime != 0 && !isForceChangeDetected)
+  {
+    lastTimerValue = millis() - timerStartTime;
+    timerStartTime = 0;
+    isForceChangeDetected = true;
   }
 }
